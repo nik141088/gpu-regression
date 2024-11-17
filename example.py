6c81e636d3be87871a560468346cf88dc960efc1
nik141088 <nikhil141088@gmail.com>
2024-11-18 00:18:27 +0530

    some changes

--- example.py
+++ example.py
@@ -1,14 +1,14 @@
 import numpy as np
 from timeit import default_timer as timer
-
+import warnings
 import cupy as cp
+
 # import cupyx
 # release all GPU memory:
 # cp._default_memory_pool.free_all_blocks()
 
 from gpu_regression import cpu_regression, gpu_regression
 
-import warnings
 warnings.filterwarnings("ignore")
 
 
@@ -46,6 +46,8 @@ print('X,Y copy to CUDA: ', round(1000*(te-ts), 2), 'ms')
 
 ts, te, beta, std_err, t_value, r2, mse = cpu_regression(X, Y)
 print('CPU (np) computation: ', round(1000*(te-ts), 2), 'ms')
+ts_2, te_2, _, _, _, _, _ = cpu_regression(X, Y)
+print('CPU (np) computation (2nd time): ', round(1000*(te_2-ts_2), 2), 'ms')
 
 ts, te, d_beta, d_std_err, d_t_value, d_r2, d_mse = gpu_regression(d_X, d_Y)
 print('GPU (cp) computation: ', round(1000*(te-ts), 2), 'ms')
@@ -53,7 +55,7 @@ ts_2, te_2, _, _, _, _, _ = gpu_regression(d_X, d_Y)
 print('GPU (cp) computation (2nd time): ', round(1000*(te_2-ts_2), 2), 'ms')
 
 
-all_close = np.allclose(beta, d_beta) * np.allclose(mse, d_mse) * np.allclose(std_err, d_std_err) * np.allclose(tval, d_tval) * np.allclose(r2, d_r2)
+all_close = np.allclose(beta, d_beta) * np.allclose(mse, d_mse) * np.allclose(std_err, d_std_err) * np.allclose(t_value, d_t_value) * np.allclose(r2, d_r2)
 print('all_close: ', all_close)
 
 del X, Y, d_X, d_Y
